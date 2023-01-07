3e201321750abf26e758e027649b6ca4ceef1613
vizucode <havisikkubaru@gmail.com>
2023-01-07 12:15:37 +0700

    feat(usecases/telegram..go): create platform telegram

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"amadeus.tele.ai/repositories/api"
+	uc "amadeus.tele.ai/usecases"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// wg := new(sync.WaitGroup)
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// initializing chai
+	chai := api.NewchaiML(os.Getenv("DEV_KEY"), os.Getenv("DEV_UID"), os.Getenv("URL_TARGET"))
+
+	// initializing usecase bot
+	teleBot := uc.NewTelegram(chai, os.Getenv("BOT_API_KEY"), false)
+
+	// starting the bot
+	// wg.Add(1)
+	start(teleBot)
+	// wg.Wait()
+}
+
+func start(chat uc.PlatformUC) {
+	chat.Chat()
 }
